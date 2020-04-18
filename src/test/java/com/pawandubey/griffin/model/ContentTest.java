f1f92024cd40c8e4028cf7a0306a0e94ca0ccd11
Dima Volykhin <dima.v@ohmsd.com>
2020-04-18 14:05:50 +0300

    fix tests

--- src/test/java/com/pawandubey/griffin/model/ContentTest.java
+++ src/test/java/com/pawandubey/griffin/model/ContentTest.java
@@ -1,14 +1,22 @@
 package com.pawandubey.griffin.model;
 
 import com.pawandubey.griffin.Data;
+import com.pawandubey.griffin.DirectoryStructure;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
 
 class ContentTest {
 
+	@TempDir
+	File anotherTempDir;
+
 	@Test
 	void parse() {
+		DirectoryStructure.create(anotherTempDir.toPath());
 		new Data();
 		final Parsable post = Content.parse("test", "layout = \"post\"\n" +
 				"title = \"Middle of the Linked List\"\n" +
