71d29bf2b24ad1209b0fcaf064ff04417316e06f
Dima Volykhin <dima.v@ohmsd.com>
2020-04-18 16:40:50 +0300

    add matrix strategy for build

--- make-release.sh
+++ make-release.sh
@@ -6,10 +6,10 @@ NAME=griffin-$(git tag -l | tail -n 1).zip
 ./gradlew build
 ./gradlew nativeImage -P com.palantir.graal.cache.dir=/tmp
 
-cp build/graal/griffin release/
+#cp build/graal/griffin release/
 
 #zip the contents of release directory
-cd release && zip -r $NAME .
+#cd release && zip -r $NAME .
 #cd .. && mv release/$NAME .
 #
 #rm release/griffin
